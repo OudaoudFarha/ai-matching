8cc9bbf4fca5c425bcc3f25ad3773a8da959b8ae
Farha Oudaoud <farhaoudaoud@gmail.com>
2026-01-21 00:08:09 +0100

    fix

--- backend/resume-service/src/test/java/com/resume/resume_service/HealthControllerTest.java
+++ backend/resume-service/src/test/java/com/resume/resume_service/HealthControllerTest.java
@@ -6,14 +6,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static javax.swing.UIManager.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@ActiveProfiles("test")
-
 class HealthControllerTest {
 
     @Autowired
