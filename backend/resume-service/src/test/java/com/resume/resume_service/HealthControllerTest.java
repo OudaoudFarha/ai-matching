e9a64b12298e5d2c880f6b3a1e36504346192198
Farha Oudaoud <farhaoudaoud@gmail.com>
2026-01-21 01:01:31 +0100

    fix

--- backend/resume-service/src/test/java/com/resume/resume_service/HealthControllerTest.java
+++ backend/resume-service/src/test/java/com/resume/resume_service/HealthControllerTest.java
@@ -1,14 +1,16 @@
 package com.resume.resume_service;
 
+import com.resume.resume_service.Services.MinioService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -17,6 +19,9 @@ class HealthControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private MinioService minioService; // ✅ empêche l'erreur "NoSuchBeanDefinition"
 
     @Test
     void health_should_return_200() throws Exception {
